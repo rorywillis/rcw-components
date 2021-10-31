7c7a03829c2cdd4623925287e227be21acc3ba85
Rory Willis <rorywillis@gmail.com>
2021-10-31 13:11:58 -0600

    Fix type declarations

---
+++ dist/Card/Card.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { CardProps } from "../types";
+declare const Card: (props: CardProps) => JSX.Element;
+export default Card;
+//# sourceMappingURL=Card.d.ts.map
---
+++ dist/CardGrid/CardGrid.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { CardGridProps } from "../types";
+declare const CardGrid: (props: CardGridProps) => JSX.Element | null;
+export default CardGrid;
+//# sourceMappingURL=CardGrid.d.ts.map
