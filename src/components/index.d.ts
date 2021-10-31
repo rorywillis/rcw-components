5893ae4142b5c9c3000dd227314b39616bce0089
Rory Willis <rorywillis@gmail.com>
2021-10-31 13:32:36 -0600

    Fix

---
+++ src/components/index.d.ts
@@ -0,0 +1,5 @@
+import Card from "./Card";
+import CardGrid from "./CardGrid";
+import { CardProps, CardGridProps, CardGridItem, WithLinkProps } from "./types";
+export { Card, CardGrid, CardProps, CardGridProps, WithLinkProps, CardGridItem, };
+//# sourceMappingURL=index.d.ts.map
