4c75a93633360fea39091a2fb10e14fa908c9dd2
Rory Willis <rorywillis@gmail.com>
2021-10-31 14:09:29 -0600

    Remove types

--- src/stories/Card.stories.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-declare const _default: ComponentMeta<(props: import("../components").CardProps) => JSX.Element>;
-export default _default;
-export declare const Basic: ComponentStory<(props: import("../components").CardProps) => JSX.Element>;
-export declare const WithTitle: ComponentStory<(props: import("../components").CardProps) => JSX.Element>;
-export declare const WithImage: ComponentStory<(props: import("../components").CardProps) => JSX.Element>;
-export declare const WithUrl: ComponentStory<(props: import("../components").CardProps) => JSX.Element>;
-//# sourceMappingURL=Card.stories.d.ts.map
--- src/stories/CardGrid.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-declare const _default: ComponentMeta<(props: import("../components").CardGridProps) => JSX.Element | null>;
-export default _default;
-export declare const Basic: ComponentStory<(props: import("../components").CardGridProps) => JSX.Element | null>;
-//# sourceMappingURL=CardGrid.stories.d.ts.map
